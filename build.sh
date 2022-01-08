aff5e4a4d282a9f532067673a31b9d1b396f893b
Gurhan Kokcu <gurhankokcu@gmail.com>
2022-01-08 23:28:11 +0000

    add copy config command to build script

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -fr ./bin/*
 mkdir ./bin/linux
 env GOOS=linux GOARCH=amd64 go build
 mv ./proxy ./bin/linux/proxy
+cp ./config.json ./bin/linux/config.json
 cp ./admin.html ./bin/linux/admin.html
 cp ./layout.html ./bin/linux/layout.html
 cp ./unauthorized.html ./bin/linux/unauthorized.html
@@ -12,6 +13,7 @@ cp ./unauthorized.html ./bin/linux/unauthorized.html
 mkdir ./bin/mac
 env GOOS=darwin GOARCH=amd64 go build
 mv ./proxy ./bin/mac/proxy
+cp ./config.json ./bin/linux/config.json
 cp ./admin.html ./bin/mac/admin.html
 cp ./layout.html ./bin/mac/layout.html
 cp ./unauthorized.html ./bin/mac/unauthorized.html
